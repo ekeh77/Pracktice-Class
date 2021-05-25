65dcfc46d415a4ac2bf667caa04b5465f201ae87
ekeh77 <74358756+ekeh77@users.noreply.github.com>
2021-05-25 17:25:33 +0900

    20210525_Java

---
+++ Java/0525/20210525_02/src/PKG_Car/Sedan.java
@@ -0,0 +1,13 @@
+package PKG_Car;
+
+public class Sedan extends Car{
+
+	public Sedan(String carName, int maxSpeed) {
+		super(carName, maxSpeed);
+	}
+	
+	public void getSedan() {
+		System.out.println("세단만의 특징");
+	}
+	
+}
